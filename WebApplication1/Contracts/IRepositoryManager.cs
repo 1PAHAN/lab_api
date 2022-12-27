7018d3c0608caa1e9b50f3623e731ac2f71b6455
1PAHAN <95417203+1PAHAN@users.noreply.github.com>
2022-12-27 20:43:23 +0300

    lab3

---
+++ WebApplication1/Contracts/IRepositoryManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts
+{
+    public interface IRepositoryManager
+    {
+        ICompanyRepository Company { get; }
+        IEmployeeRepository Employee { get; }
+        IProductRepository Product { get; }
+        ICustomerRepository Customer { get; }
+        void Save();
+    }
+}
---
+++ WebApplication1/Repository/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+
+        }
+
+        public IEnumerable<Customer> GetAllCustomers(bool trackChanges)
+        {
+            return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+        }
+    }
+}
