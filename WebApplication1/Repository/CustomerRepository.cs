c798af6f47d67f4a731201a2a97094aa78e9b318
1PAHAN <95417203+1PAHAN@users.noreply.github.com>
2022-12-27 20:45:43 +0300

    lab4

---
+++ WebApplication1/Repository/CustomerRepository.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+
+        }
+
+        public IEnumerable<Customer> GetAllCustomers(Guid productId, bool trackChanges)
+        {
+            return FindByCondition(e => e.ProductId.Equals(productId), trackChanges)
+                .OrderBy(e => e.Name);
+        }
+
+        public Customer GetCustomer(Guid productId, Guid customerId, bool trackChanges)
+        {
+            return FindByCondition(e => e.ProductId.Equals(productId) && e.Id.Equals(customerId),
+                trackChanges).SingleOrDefault();
+        }
+    }
+}
---
+++ WebApplication1/Repository/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+
+        }
+
+        public IEnumerable<Product> GetAllProducts(bool trackChanges)
+        {
+            return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+        }
+
+        public Product GetProduct(Guid id, bool trackChanges)
+        {
+            return FindByCondition(e => e.Id.Equals(id), trackChanges)
+                .SingleOrDefault();
+        }
+    }
+}
